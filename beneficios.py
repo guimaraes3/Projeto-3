f299fd92e28bfc22265a50245e759bef48e80ce3
guimaraes <00001109951024sp@al.educacao.sp.gov.br>
2024-05-03 21:02:16 -0300

    obeservações de salvamento

---
+++ beneficios.py
@@ -0,0 +1,7 @@
+trabalho = float(input("digite seu tempo de trabalho: "))
+if trabalho <= 5:
+   print("terá um aumento no vale refeisão.")
+elif trabalho > 5 and trabalho <= 10:
+   print("reajuste de 10% no salário.")
+else:
+   print("participação da festa de comemoração. ")
